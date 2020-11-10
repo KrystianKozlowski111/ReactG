d56f9b10b8c9b254e516ef7162e9e6f344d07895
KrystianKozlowski <krystiankozlowski111@gmail.com>
2020-11-10 14:12:44 +0100

    About

---
+++ galaxy/src/components/About/About.style.js
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+export const AboutTextContainer = styled.section`
+  display: flex;
+  align-items: left;
+  justify-content: flex-start;
+  flex-direction: column;
+  max-width: 474px;
+  padding-left: 126px;
+`;
+export const AboutContainer = styled.section`
+  display: flex;
+  align-items: left;
+  justify-content: space-between;
+  flex-direction: row;
+  max-width: 1121px;
+`;
+export const AboutHeader = styled.h1`
+  //styleName: Big Title;
+  font-family: Fira Sans;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 50px;
+  letter-spacing: -1px;
+  text-align: left;
+`;
+export const AboutText = styled.p`
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 28px;
+  color: #222222;
+  opacity: 0.7;
+`;
+export const Placeholder = styled.div`
+  position: relative;
+`;
+export const UserContainer = styled.section`
+  margin-top: 51px;
+  display: flex;
+  align-items: left;
+  justify-content: flex-start;
+  max-width: 285px;
+`;
+export const User = styled.div`
+  position: relative;
+`;
+export const UserTextContainer = styled.section`
+  display: flex;
+  align-items: left;
+  justify-content: flex-start;
+  flex-direction: column;
+`;
+export const UserHeader = styled.h1`
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 32px;
+  color: #222222;
+`;
+export const UserText = styled.p`
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 28px;
+  color: #222222;
+  opacity: 0.7;
+`;
