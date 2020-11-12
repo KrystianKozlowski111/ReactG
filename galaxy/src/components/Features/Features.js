6dc135ed221155601fb41fdee8e4fe992ecaa33b
KrystianKozlowski <krystiankozlowski111@gmail.com>
2020-11-12 13:54:23 +0100

    Features done

--- galaxy/src/components/Features/Features.js
+++ galaxy/src/components/Features/Features.js
@@ -1,18 +1,64 @@
 import React from 'react';
-import { Header, TextContainer, Text } from './Features.style';
-
+import {
+  Header,
+  FeatureContainer,
+  TextContainer,
+  Text,
+  Placeholder1,
+  Placeholder2,
+  Icon,
+  RightContainer,
+  IconText,
+  IconTextContainer,
+  IconContainer,
+} from './Features.style';
+import PlaceholderFeatures1 from '../../assets/images/PlaceholderFeatures1.svg';
+import PlaceholderFeatures2 from '../../assets/images/PlaceholderFeatures2.svg';
+import Icon100 from '../../assets/images/Icon100.svg';
+import Bagicon from '../../assets/images/Bagicon.svg';
 const Features = () => {
   return (
-    <>
-      <TextContainer>
-        <Header>Well thought with super easy to use</Header>
-        <Text>
-          As we approached the palace I could see through the great windows of
-          the first floor the brilliantly illuminated audience chamber of Than
-          Kosis. The immense hall was crowded with nobles and their women.
-        </Text>
-      </TextContainer>
-    </>
+    <FeatureContainer>
+      <Placeholder1>
+        {' '}
+        <img
+          src={PlaceholderFeatures1}
+          className="App-PlaceholderFeatures1"
+          alt="PlaceholderFeatures1"
+        />
+      </Placeholder1>
+      <Placeholder2>
+        <img
+          src={PlaceholderFeatures2}
+          className="App-PlaceholderFeatures2"
+          alt="PlaceholderFeatures2"
+        />
+      </Placeholder2>
+      <RightContainer>
+        <TextContainer>
+          <Header>Well thought with super easy to use</Header>
+          <Text>
+            As we approached the palace I could see through the great windows of
+            the first floor the brilliantly illuminated audience chamber of Than
+            Kosis. The immense hall was crowded with nobles and their women.
+          </Text>
+        </TextContainer>
+        <IconContainer>
+          <IconTextContainer>
+            <Icon>
+              <img src={Icon100} className="App-Icon100" alt="Icon100" />
+            </Icon>
+            <IconText>Ultimate Result</IconText>
+          </IconTextContainer>
+          <IconTextContainer>
+            <Icon>
+              <img src={Bagicon} className="App-Bagicon" alt="Bagicon" />
+            </Icon>
+            <IconText>Minimal Design</IconText>
+          </IconTextContainer>
+        </IconContainer>
+      </RightContainer>
+    </FeatureContainer>
   );
 };
 
--- galaxy/src/components/Features/Features.style.js
+++ galaxy/src/components/Features/Features.style.js
@@ -5,9 +5,9 @@ export const Header = styled.h1`
   font-weight: 600;
   font-size: 40px;
   line-height: 50px;
-
+  margin-bottom: 0px;
   letter-spacing: -1px;
-
+  margin-top: 38px;
   color: #222222;
 `;
 export const Text = styled.p`
@@ -16,13 +16,63 @@ export const Text = styled.p`
   font-weight: normal;
   font-size: 18px;
   line-height: 28px;
+  margin-top: 13px;
   color: #222222;
   opacity: 0.7;
 `;
 export const TextContainer = styled.section`
   display: flex;
+  justify-content: flex-start;
 
-  justify-content: flex-end;
   flex-direction: column;
   max-width: 475px;
+`;
+export const FeatureContainer = styled.section`
+  display: flex;
+
+  align-items: left;
+`;
+export const RightContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+`;
+export const Placeholder1 = styled.div`
+  position: relative;
+  margin-right: 190px;
+`;
+export const Placeholder2 = styled.div`
+  position: absolute;
+  z-index: 8;
+  top: 5%;
+  left: 10%;
+
+  filter: drop-shadow(30px 80px 180px rgba(0, 0, 0, 0.25));
+`;
+export const IconTextContainer = styled.section`
+  display: flex;
+  justify-content: flex-start;
+  margin-bottom: 30px;
+`;
+export const IconText = styled.p`
+  margin-left: 29px;
+  font-family: Fira Sans, sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 24px;
+
+  letter-spacing: 4px;
+  text-transform: uppercase;
+
+  color: #000000;
+`;
+export const Icon = styled.div`
+  position: relative;
+`;
+export const IconContainer = styled.section`
+  margin-top: 51px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
