03de3b250a96b902b5f46393aed1df824c300d53
KrystianKozlowski <krystiankozlowski111@gmail.com>
2020-11-15 17:12:08 +0100

    .

--- galaxy/src/views/HomePage.js
+++ galaxy/src/views/HomePage.js
@@ -1,24 +1,6 @@
 import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import Hero from '../components/Hero';
-import About from '../components/About';
-import Clients from '../components/Clients';
-import Testimonials from '../components/Testimonials';
-import Features from '../components/Features';
-import Works from '../components/Works';
+
 function HomePage() {
-  return (
-    <>
-      <Header />
-      <Hero />
-      <About />
-      <Features />
-      <Works />
-      <Testimonials />
-      <Clients />
-      <Footer />
-    </>
-  );
+  return <></>;
 }
 export default HomePage;
